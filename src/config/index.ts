fa476f9912a111e58d23e8279052107bbbbe6a27
Emmanuel Meinike <emathegreat@gmail.com>
2018-02-24 21:25:00 +0100

    reset settings

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,14 @@
 import HSLColor from './../components/HSLColor';
 
 export const BASE_COLORS: Array<HSLColor> = [
-    //new HSLColor(270, 90, 80),
-    //new HSLColor(318, 90, 80),
-    //new HSLColor(210, 90, 80),
+    new HSLColor(270, 90, 80),
+    new HSLColor(318, 90, 80),
+    new HSLColor(210, 90, 80),
     new HSLColor(100, 90, 80)
 ];
 
 export const COLORS: Array<HSLColor> = [];
 export const CIRCLE_MARGIN: number = 10;
 export const MAX_ITERATIONS: number = 50000;
-export const MIN_RADIUS: number = 25;
-export const MAX_RADIUS: number = 50;
+export const MIN_RADIUS: number = 10;
+export const MAX_RADIUS: number = 100;
